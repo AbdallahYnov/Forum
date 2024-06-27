d5139dcae71cf4335ecd0274da144c23dc59770f
AbdallahYnov <abdallah.merad@ynov.com>
2024-06-27 10:52:52 +0200

    Update users.js

--- API/routes/users.js
+++ API/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
+router.post('/register', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
